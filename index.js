0c660b6b6fb3e25a7a1bd2ad8576f298e0bf1be3
zakarya ghanai <zakarya.ghanai@talenteo.com>
2023-03-11 12:42:36 +0100

    move server entry point to index

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-require('./app/config/initializer/server');
+'use strict'
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+
+const schema = require('./app/graphql/schema')
+
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+}));
+
+
+// Server Port
+const port = process.env.PORT || 7887;
+app.set('port', port);
+
+// Listen for server
+app.listen(port, () => {
+    console.log(`server starting on localhost:${port}`)
+});
