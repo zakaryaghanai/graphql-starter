ff6f2910cd321a5615bb2943afea2a902a7bf45f
zakarya ghanai <zakarya.ghanai@talenteo.com>
2023-03-11 17:09:18 +0100

    add vercel config file

--- index.js
+++ index.js
@@ -7,14 +7,19 @@ const schema = require('./app/graphql/schema')
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+app.use('/api/graphql', graphqlHTTP({
     schema,
     graphiql: true,
 }));
 
+app.use('/api', (req, res) => {
+    res.send('Hey this is my API running 🥳')
+});
+
+
 
 // Server Port
-const port = process.env.PORT || 7887;
+const port = process.env.PORT || 5000;
 app.set('port', port);
 
 // Listen for server
